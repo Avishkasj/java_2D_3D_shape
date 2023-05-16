45a665f2235b4a24ce3ada5342657bea03611080
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-16 22:16:59 +0530

    load 2d after button clicked

---
+++ project/src/all2d.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class all2d extends JFrame {
+
+    private JPanel mypanel2;
+
+    public all2d(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mypanel2);
+        this.pack();
+    }
+
+
+
+
+
+
+}
+
--- project/src/dashboard.java
+++ project/src/dashboard.java
@@ -1,15 +1,31 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class dashboard extends JFrame {
 
 
     private JPanel mypanel;
+    private JButton a2DButton;
+    private JButton a3DButton;
+
 
     public dashboard(String title){
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mypanel);
         this.pack();
+
+        a2DButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dashboard.super.dispose();
+                JFrame frame2= new all2d("All 2D");
+                frame2.setVisible(true);
+                frame2.setSize(1000, 600);
+
+            }
+        });
     }
 
     public static void main(String[] args) {
