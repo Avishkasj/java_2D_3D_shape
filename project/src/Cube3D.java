483bbea5230b5a7378493fdba0e5ead23629ebb8
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-18 13:39:15 +0530

    cube and round with shadow

---
+++ project/src/Cube3D.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Cube3D extends JFrame {
+    private int size;
+    private Color color;
+    private boolean hasShadow;
+    private Color shadowColor;
+
+    public Cube3D(int size, Color color, boolean hasShadow, Color shadowColor) {
+        this.size = size;
+        this.color = color;
+        this.hasShadow = hasShadow;
+        this.shadowColor = shadowColor;
+
+        setTitle("3D Cube Generator");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        int cubeSize = size * 20; // Adjust the scaling factor as needed
+
+        int topLeftX = centerX - cubeSize / 2;
+        int topLeftY = centerY - cubeSize / 2;
+
+        // Draw the shadow
+        if (hasShadow) {
+            int shadowOffset = size * 10; // Adjust the shadow offset as needed
+
+            g2d.setColor(shadowColor);
+            g2d.fillRect(topLeftX + shadowOffset, topLeftY + shadowOffset, cubeSize, cubeSize);
+        }
+
+        // Draw the front face
+        g2d.setColor(color);
+        g2d.fillRect(topLeftX, topLeftY, cubeSize, cubeSize);
+
+        // Draw the top face
+        g2d.setColor(color.brighter());
+        g2d.fillPolygon(new int[]{topLeftX, topLeftX + cubeSize, topLeftX + cubeSize, topLeftX},
+                new int[]{topLeftY, topLeftY, topLeftY - cubeSize / 2, topLeftY - cubeSize / 2}, 4);
+
+        // Draw the right face
+        g2d.setColor(color.darker());
+        g2d.fillPolygon(new int[]{topLeftX + cubeSize, topLeftX + cubeSize, topLeftX + cubeSize, topLeftX + cubeSize},
+                new int[]{topLeftY, topLeftY - cubeSize / 2, topLeftY - cubeSize / 2 + cubeSize, topLeftY + cubeSize}, 4);
+
+        g2d.dispose();
+    }
+
+    public static void main(String[] args) {
+        int size = Integer.parseInt(JOptionPane.showInputDialog("Enter the size of the cube:"));
+        Color color = JColorChooser.showDialog(null, "Choose a color", Color.WHITE);
+
+        int choice = JOptionPane.showConfirmDialog(null, "Do you want to add a shadow?", "Shadow", JOptionPane.YES_NO_OPTION);
+        boolean hasShadow = (choice == JOptionPane.YES_OPTION);
+
+        Color shadowColor = null;
+        if (hasShadow) {
+            shadowColor = JColorChooser.showDialog(null, "Choose a shadow color", Color.BLACK);
+        }
+
+        Cube3D cubeGenerator = new Cube3D(size, color, hasShadow, shadowColor);
+        cubeGenerator.setVisible(true);
+    }
+}
---
+++ project/src/Sphere3D.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Sphere3D extends JFrame {
+    private int size;
+    private Color color;
+    private boolean hasShadow;
+    private Color shadowColor;
+
+    public Sphere3D(int size, Color color, boolean hasShadow, Color shadowColor) {
+        this.size = size;
+        this.color = color;
+        this.hasShadow = hasShadow;
+        this.shadowColor = shadowColor;
+
+        setTitle("3D Sphere Generator");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new CustomPanel();
+        add(panel);
+    }
+
+    private class CustomPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D) g;
+
+            int centerX = getWidth() / 2;
+            int centerY = getHeight() / 2;
+
+            int sphereRadius = size * 10; // Adjust the scaling factor as needed
+
+            int topLeftX = centerX - sphereRadius;
+            int topLeftY = centerY - sphereRadius;
+
+            // Draw the shadow
+            if (hasShadow) {
+                int shadowOffset = size * 8; // Adjust the shadow offset as needed
+                int shadowRadius = sphereRadius / 2;
+                int shadowTopLeftX = topLeftX + shadowOffset;
+                int shadowTopLeftY = topLeftY + shadowOffset;
+
+                g2d.setColor(shadowColor);
+                g2d.fillOval(shadowTopLeftX, shadowTopLeftY, 2 * shadowRadius, 2 * shadowRadius);
+            }
+
+            // Draw the sphere
+            g2d.setColor(color);
+            g2d.fillOval(topLeftX, topLeftY, 2 * sphereRadius, 2 * sphereRadius);
+
+            g2d.dispose();
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = Integer.parseInt(JOptionPane.showInputDialog("Enter the size of the sphere:"));
+        Color color = JColorChooser.showDialog(null, "Choose a color", Color.WHITE);
+
+        int choice = JOptionPane.showConfirmDialog(null, "Do you want to add a shadow?", "Shadow", JOptionPane.YES_NO_OPTION);
+        boolean hasShadow = (choice == JOptionPane.YES_OPTION);
+
+        Color shadowColor = null;
+        if (hasShadow) {
+            shadowColor = JColorChooser.showDialog(null, "Choose a shadow color", Color.BLACK);
+        }
+
+        Sphere3D sphereGenerator = new Sphere3D(size, color, hasShadow, shadowColor);
+        sphereGenerator.setVisible(true);
+    }
+}
